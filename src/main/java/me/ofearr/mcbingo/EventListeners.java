7b909a409d6f63d93f11cd6baffd3ffab0908636
oFearr <fearadinopvp@hotmail.com>
2021-01-23 21:02:50 +0000

    Finished project core aspects.

--- src/main/java/me/ofearr/mcbingo/EventListeners.java
+++ src/main/java/me/ofearr/mcbingo/EventListeners.java
@@ -1,24 +1,24 @@
 package me.ofearr.mcbingo;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerAttemptPickupItemEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
 public class EventListeners implements Listener {
-
-    HashMap<Location, ItemStack> cheatBreaker = new HashMap<>();
 
     public static String TranslateColour(String text){
         String translatedColour = ChatColor.translateAlternateColorCodes('&', text);
@@ -27,33 +27,131 @@ public class EventListeners implements Listener {
     }
 
 
+    @EventHandler
+    public void EventPVP(EntityDamageByEntityEvent e){
+        if(MCBingo.gameActive == false) return;
+        if(!(e.getEntity() instanceof Player)) return;
+
+        e.setCancelled(true);
+        e.getDamager().sendMessage(TranslateColour("&8[&e&lBingo&8] >> &cYou cannot damage other players during this game!"));
+    }
 
     @EventHandler
-    public void ItemCollectEvent(PlayerAttemptPickupItemEvent e){
+    public void EventDrop(PlayerDropItemEvent e){
+
         if(MCBingo.gameActive == false) return;
-        Material mat = e.getItem().getItemStack().getType();
-        Player player = e.getPlayer();
+        if(e.getItemDrop() == null) return;
 
-        ArrayList<String> splitObjectives = new ArrayList<>(Arrays.asList(MCBingo.playerObjectives.get(player.getUniqueId()).split(", ")));
+        if(!(e.getItemDrop().getItemStack().getItemMeta().hasLore())){
 
-        for(int i = 0; i < splitObjectives.size(); i++){
-            if(splitObjectives.get(i) == mat.toString()){
-                splitObjectives.remove(i);
-            }
+            ItemMeta im = e.getItemDrop().getItemStack().getItemMeta();
+
+            ArrayList<String> excludeLore = new ArrayList<>();
+
+            excludeLore.add(" ");
+            excludeLore.add(TranslateColour("&cExcluded from Game!"));
+            excludeLore.add(TranslateColour("&cThis item cannot be used"));
+            excludeLore.add(TranslateColour("&cto complete a bingo objective!"));
+            excludeLore.add(" ");
+            excludeLore.add(TranslateColour("&c(Can still be used for crafting!)"));
+            im.setLore(excludeLore);
+
+            e.getItemDrop().getItemStack().setItemMeta(im);
+        } else{
+            return;
         }
 
-        if(splitObjectives.size() == 0){
-            for (Player p : Bukkit.getOnlinePlayers()) {
-                p.sendTitle(TranslateColour("&a&lGame Over!"), TranslateColour("&aThe game has been won by " + player.getName() + "!"));
-                p.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aThe game has been won by " + player.getName() + "!"));
-            }
-            MCBingo.gameActive = false;
-        }
 
     }
 
     @EventHandler
-    public void SmeltCraftChestItemEvent(InventoryClickEvent e){
+    public void ItemCollectEvent(PlayerAttemptPickupItemEvent e){
+        if(MCBingo.gameActive == false) return;
+        if(e.getItem() == null) return;
+        Material mat = e.getItem().getItemStack().getType();
+        Player player = e.getPlayer();
+
+        if(!(e.getItem().getItemStack().hasItemMeta())){
+            ArrayList<String> splitObjectives = new ArrayList<>(Arrays.asList(MCBingo.playerObjectives.get(player.getUniqueId()).split(", ")));
+
+            for(int i = 0; i < splitObjectives.size(); i++) {
+                if (splitObjectives.get(i).equalsIgnoreCase(mat.toString())) {
+                    splitObjectives.remove(i);
+
+                    StringBuilder builder = new StringBuilder();
+                    for (String cVal : splitObjectives) {
+                        builder.append(cVal + ", ");
+                    }
+
+                    String updated = "";
+                    if(builder.toString().contains(", ")){
+                        updated = builder.toString().substring(0, builder.length() - 2);
+                    } else {
+                        updated = builder.toString();
+                    }
+
+                    MCBingo.playerObjectives.remove(player.getUniqueId());
+                    MCBingo.playerObjectives.put(player.getUniqueId(), updated);
+
+                    if(splitObjectives.size() == 0){
+                        MCBingo.GameWon(player);
+                    } else {
+                        player.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aYou've complete one of your bingo objectives!"));
+                        player.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aCheck your /bingocard to see what's next!"));
+                    }
+
+                    break;
+                }
+            }
+        }  else{
+            return;
+        }
+
+        }
+
+    @EventHandler
+    public void BlockBreak(BlockBreakEvent e){
+        if(MCBingo.gameActive == false) return;
+        Material mat = e.getBlock().getType();
+        Player player = e.getPlayer();
+
+        ArrayList<String> splitObjectives = new ArrayList<>(Arrays.asList(MCBingo.playerObjectives.get(player.getUniqueId()).split(", ")));
+
+        for(int i = 0; i < splitObjectives.size(); i++) {
+            if (splitObjectives.get(i).equalsIgnoreCase(mat.toString())) {
+                splitObjectives.remove(i);
+
+                StringBuilder builder = new StringBuilder();
+                for (String cVal : splitObjectives) {
+                    builder.append(cVal + ", ");
+                }
+
+                String updated = "";
+                if(builder.toString().contains(", ")){
+                    updated = builder.toString().substring(0, builder.length() - 2);
+                } else {
+                    updated = builder.toString();
+                }
+
+                MCBingo.playerObjectives.remove(player.getUniqueId());
+                MCBingo.playerObjectives.put(player.getUniqueId(), updated);
+
+                if(splitObjectives.size() == 0){
+                    MCBingo.GameWon(player);
+                } else {
+                    player.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aYou've complete one of your bingo objectives!"));
+                    player.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aCheck your /bingocard to see what's next!"));
+                }
+
+            }
+        }
+
+
+    }
+
+    @EventHandler
+    public void SmeltChestItemEvent(InventoryClickEvent e){
+        if(MCBingo.gameActive == false) return;
         ItemStack item = e.getCursor();
         if(item == null) return;
         if(e.getSlotType().equals(InventoryType.SlotType.OUTSIDE)) return;
@@ -63,45 +161,137 @@ public class EventListeners implements Listener {
 
             ArrayList<String> splitObjectives = new ArrayList<>(Arrays.asList(MCBingo.playerObjectives.get(player.getUniqueId()).split(", ")));
 
-            for(int i = 0; i < splitObjectives.size(); i++){
-                if(splitObjectives.get(i) == mat.toString()){
+
+            for(int i = 0; i < splitObjectives.size(); i++) {
+                if (splitObjectives.get(i).equalsIgnoreCase(mat.toString())) {
                     splitObjectives.remove(i);
+
+                    StringBuilder builder = new StringBuilder();
+                    for (String cVal : splitObjectives) {
+                        builder.append(cVal + ", ");
+                    }
+
+                    String updated = "";
+                    if(builder.toString().contains(", ")){
+                        updated = builder.toString().substring(0, builder.length() - 2);
+                    } else {
+                        updated = builder.toString();
+                    }
+                    MCBingo.playerObjectives.remove(player.getUniqueId());
+                    MCBingo.playerObjectives.put(player.getUniqueId(), updated);
+
+                    if(splitObjectives.size() == 0){
+                        MCBingo.GameWon(player);
+                    } else {
+                        player.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aYou've complete one of your bingo objectives!"));
+                        player.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aCheck your /bingocard to see what's next!"));
+                    }
+                    break;
+
                 }
             }
 
-            if(splitObjectives.size() == 0){
-                for (Player p : Bukkit.getOnlinePlayers()) {
-                    p.sendTitle(TranslateColour("&a&lGame Over!"), TranslateColour("&aThe game has been won by " + player.getName() + "!"));
-                    p.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aThe game has been won by " + player.getName() + "!"));
-                }
-                MCBingo.gameActive = false;
-            }
+        }
 
-        } else if(e.getSlotType().equals(InventoryType.SlotType.CONTAINER)){
+        if(e.getView().getTitle().equalsIgnoreCase(TranslateColour("&8[&e&lBingo Card&8]"))){
+            if(e.getSlotType() == InventoryType.SlotType.OUTSIDE) return;
+            if(e.getCurrentItem() == null) return;
+            e.setCancelled(true);
+        }
+
+        if(e.getSlotType().equals(InventoryType.SlotType.CONTAINER) && !(e.getView().getTitle().equalsIgnoreCase(TranslateColour("&8[&e&lBingo Card&8]")))){
+            if(e.getCurrentItem() == null) return;
             Material mat = e.getCurrentItem().getType();
             Player player = (Player) e.getWhoClicked();
-            Location loc = e.getClickedInventory().getLocation();
 
-            if(cheatBreaker.containsKey(loc)) return;
 
-            ArrayList<String> splitObjectives = new ArrayList<>(Arrays.asList(MCBingo.playerObjectives.get(player.getUniqueId()).split(", ")));
+            if(!(e.getCurrentItem().hasItemMeta())){
+                ArrayList<String> splitObjectives = new ArrayList<>(Arrays.asList(MCBingo.playerObjectives.get(player.getUniqueId()).split(", ")));
 
-            for(int i = 0; i < splitObjectives.size(); i++){
-                if(splitObjectives.get(i) == mat.toString()){
-                    splitObjectives.remove(i);
+                for(int i = 0; i < splitObjectives.size(); i++) {
+                    if (splitObjectives.get(i).equalsIgnoreCase(mat.toString())) {
+                        splitObjectives.remove(i);
+
+                        StringBuilder builder = new StringBuilder();
+                        for (String cVal : splitObjectives) {
+                            builder.append(cVal + ", ");
+                        }
+
+                        String updated = "";
+                        if(builder.toString().contains(", ")){
+                            updated = builder.toString().substring(0, builder.length() - 2);
+                        } else {
+                            updated = builder.toString();
+                        }
+
+                        MCBingo.playerObjectives.remove(player.getUniqueId());
+                        MCBingo.playerObjectives.put(player.getUniqueId(), updated);
+
+                        if(splitObjectives.size() == 0){
+                            MCBingo.GameWon(player);
+                        } else {
+                            player.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aYou've complete one of your bingo objectives!"));
+                            player.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aCheck your /bingocard to see what's next!"));
+                        }
+                        break;
+
+                    }
                 }
+
+                ItemMeta im = e.getCurrentItem().getItemMeta();
+
+                ArrayList<String> excludeLore = new ArrayList<>();
+
+                excludeLore.add(" ");
+                excludeLore.add(TranslateColour("&cExcluded from Game!"));
+                excludeLore.add(TranslateColour("&cThis item cannot be used"));
+                excludeLore.add(TranslateColour("&cto complete a bingo objective!"));
+                excludeLore.add(" ");
+                excludeLore.add(TranslateColour("&c(Can still be used for crafting!)"));
+                im.setLore(excludeLore);
+
+                e.getCurrentItem().setItemMeta(im);
+            } else{
+                return;
             }
 
-            if(splitObjectives.size() == 0){
-                for (Player p : Bukkit.getOnlinePlayers()) {
-                    p.sendTitle(TranslateColour("&a&lGame Over!"), TranslateColour("&aThe game has been won by " + player.getName() + "!"));
-                    p.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aThe game has been won by " + player.getName() + "!"));
+
+
+        }
+
+    }
+
+    @EventHandler
+    public void CraftItem(CraftItemEvent e){
+
+        Material mat = e.getCurrentItem().getType();
+        Player player = (Player) e.getWhoClicked();
+
+        ArrayList<String> splitObjectives = new ArrayList<>(Arrays.asList(MCBingo.playerObjectives.get(player.getUniqueId()).split(", ")));
+
+
+        for(int i = 0; i < splitObjectives.size(); i++) {
+            if (splitObjectives.get(i).equalsIgnoreCase(mat.toString())) {
+                splitObjectives.remove(i);
+
+                StringBuilder builder = new StringBuilder();
+                for (String cVal : splitObjectives) {
+                    builder.append(cVal + ", ");
                 }
-                MCBingo.gameActive = false;
+                String updated = builder.toString().substring(0, builder.length() - 2);
+
+                MCBingo.playerObjectives.remove(player.getUniqueId());
+                MCBingo.playerObjectives.put(player.getUniqueId(), updated);
+
+                if(splitObjectives.size() == 0){
+                    MCBingo.GameWon(player);
+                } else {
+                    player.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aYou've complete one of your bingo objectives!"));
+                    player.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aCheck your /bingocard to see what's next!"));
+                }
+                break;
+
             }
-
-            cheatBreaker.put(loc, e.getCurrentItem());
-
         }
 
     }
--- src/main/java/me/ofearr/mcbingo/MCBingo.java
+++ src/main/java/me/ofearr/mcbingo/MCBingo.java
@@ -2,14 +2,17 @@ package me.ofearr.mcbingo;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import java.util.HashMap;
-import java.util.Random;
-import java.util.UUID;
+
+import java.util.*;
 
 public final class MCBingo extends JavaPlugin {
 
@@ -38,6 +41,57 @@ public final class MCBingo extends JavaPlugin {
         saveDefaultConfig();
     }
 
+    public Inventory BingoCardGUI(Player player){
+
+        ArrayList<String> remainingObj = new ArrayList<>(Arrays.asList(playerObjectives.get(player.getUniqueId()).split(", ")));
+        int ObjAmt = getConfig().getInt("Settings.Goal-Amount");
+        int slots = getConfig().getInt("Settings.GUI-Slots");
+
+        String completionCodes = TranslateColour("&e" + remainingObj.size() + "&f/" + "&a" + ObjAmt + " &7Remaining");
+
+        Inventory cardGUI = Bukkit.createInventory(null, slots, TranslateColour("&8[&e&lBingo Card&8]"));
+
+        ItemStack fillerItem = new ItemStack(Material.BLUE_STAINED_GLASS_PANE);
+
+        ItemMeta fillerMeta = fillerItem.getItemMeta();
+        fillerMeta.setDisplayName(TranslateColour("&8[&e&lBingo&8] >> " + completionCodes));
+
+        ArrayList<String> fillerLore = new ArrayList<>();
+
+        fillerLore.add(TranslateColour(" "));
+        fillerLore.add(TranslateColour("&aShould you encounter any bugs,"));
+        fillerLore.add(TranslateColour("&aplease report them to &noFearr&a,"));
+        fillerLore.add(TranslateColour("&a&nDemonify&a or &nOhEmily."));
+
+        fillerMeta.setLore(fillerLore);
+        fillerItem.setItemMeta(fillerMeta);
+
+        for (int i = 0; i < slots; i++){
+            cardGUI.setItem(i, fillerItem);
+        }
+
+        for (int i = 0; i < remainingObj.size(); i++){
+
+                ItemStack currentItem = new ItemStack(Material.getMaterial(String.valueOf(remainingObj.get(i))));
+
+                cardGUI.setItem(i, currentItem);
+
+        }
+
+        player.openInventory(cardGUI);
+
+        return cardGUI;
+    }
+
+    public static void GameWon(Player player){
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            p.sendTitle(TranslateColour("&a&lGame Over!"), TranslateColour("&aThe game has been won by " + player.getName() + "!"));
+            p.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aThe game has been won by " + player.getName() + "!"));
+            playerObjectives.clear();
+            gameActive = false;
+        }
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().toLowerCase().equals("startbingo")) {
@@ -48,8 +102,10 @@ public final class MCBingo extends JavaPlugin {
                 } else {
                         for (Player p : Bukkit.getOnlinePlayers()) {
 
-                            p.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aThe game has been started by " + sender.getName() + "!"));
+                            p.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aThe game countdown has been started by " + sender.getName() + "!"));
                         }
+
+                        playerObjectives.clear();
 
                         new BukkitRunnable() {
 
@@ -57,6 +113,10 @@ public final class MCBingo extends JavaPlugin {
 
                             @Override
                             public void run() {
+
+                                if(gameActive == true){
+                                    this.cancel();
+                                }
 
                                 if (gameTimer == 10) {
                                     for (Player p : Bukkit.getOnlinePlayers()) {
@@ -100,21 +160,21 @@ public final class MCBingo extends JavaPlugin {
                                     }
                                 }
 
-                                if (gameTimer == 0) {
+                                if (gameTimer <= 0) {
                                     for (Player p : Bukkit.getOnlinePlayers()) {
                                         p.sendTitle(TranslateColour("&a&lGame Starting!"), TranslateColour("&aThe game has started! Use /bingocard to view your objectives!"));
                                         p.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aThe game has started! Use /bingocard to view your objectives!"));
+                                        this.cancel();
                                     }
                                     gameActive = true;
-
                                     for(Player p : Bukkit.getOnlinePlayers()){
                                         p.getInventory().clear();
                                         String playerGoal = "";
                                         for(int i = 0; i < getConfig().getInt("Settings.Goal-Amount"); i++){
                                             Random rand = new Random();
-                                            int locIndex = rand.nextInt(getConfig().getStringList("Materials").size());
+                                            int locIndex = rand.nextInt(getConfig().getStringList("Settings.Materials").size());
 
-                                            String selectedItem = getConfig().getStringList("Materials").get(locIndex);
+                                            String selectedItem = getConfig().getStringList("Settings.Materials").get(locIndex);
 
                                             if(i < getConfig().getInt("Settings.Goal-Amount") - 1){
                                                 playerGoal = playerGoal + selectedItem + ", ";
@@ -125,10 +185,6 @@ public final class MCBingo extends JavaPlugin {
                                         }
                                         playerObjectives.put(p.getUniqueId(), playerGoal);
                                     }
-
-
-
-
 
                                 }
                                 gameTimer--;
@@ -149,7 +205,24 @@ public final class MCBingo extends JavaPlugin {
                     for(Player p : Bukkit.getOnlinePlayers()){
                         p.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &aThe game has been ended by " + sender.getName() + "!"));
                         playerObjectives.clear();
+                        gameActive = false;
                     }
+                }
+            }
+        }
+
+        if (command.getName().toLowerCase().equals("bingocard")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (!(player.hasPermission("bingo.card"))) {
+                    player.sendMessage(ChatColor.RED + "Insufficient permissions!");
+                } else {
+                    if(playerObjectives.containsKey(player.getUniqueId())){
+                        BingoCardGUI(player);
+                    } else {
+                        player.sendMessage(TranslateColour("&8[&e&lBingo&8] >> &cYou can only issue this command during a game!"));
+                    }
+
                 }
             }
         }
